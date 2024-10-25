4ed87cfc98f0acd7fbbc0960824bdf568ce43261
Jensi-Bhalala <jensibhalala@gmail.com>
2024-10-25 17:51:38 +0530

    initialcommit

---
+++ pr-8- Data table/src/pages/about/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+const About = () => {
+  return (
+    <>
+      <Header/>
+      <div className='mt-5'>
+       <h1>About Page</h1>
+      </div>
+    </>
+  )
+}
+
+export default About
---
+++ pr-8- Data table/src/pages/contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header";
+
+const Contact = () => {
+  return (
+    <>
+      <Header />
+      <div className="mt-5">
+        <h1>Contact Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
